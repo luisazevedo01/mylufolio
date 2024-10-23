78b33e1bd0720493ee755ed151ca6c7904f4c4b8
Luis Azevedo <luismiguelgomes2001@gmail.com>
2024-10-23 17:02:47 +0000

    ADD: Tech cloud, FIX: minor issues

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,21 +3,30 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { GithubIcon, LinkedinIcon, MailIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
-import {
-  IconBrandCSharp,
-  IconBrandCss3,
-  IconBrandGit,
-  IconBrandGraphql,
-  IconBrandHtml5,
-  IconBrandJavascript,
-  IconBrandNextjs,
-  IconBrandReact,
-  IconBrandTailwind,
-  IconBrandTypescript,
-} from "@tabler/icons-react";
+import IconCloud from "@/components/ui/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "react",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+];
 
 export default function Home() {
-  const dotPatterWidth = Math.floor(window.innerWidth / 3);
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <DotPattern
@@ -29,7 +38,7 @@ export default function Home() {
           `[mask-image:radial-gradient(circle_at_center,white,transparent)]`
         )}
       />
-      <div className="max-w-3xl mx-auto my-12 p-6">
+      <div className=" max-w-3xl mx-auto my-12 p-6 ">
         <h1 className="text-4xl font-bold mb-2">Hi, I'm Luís</h1>
         <h2 className="text-3xl font-semibold mb-6 bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text">
           Frontend Developer.
@@ -48,30 +57,23 @@ export default function Home() {
         </div>
 
         <h3 className="mb-8 text-gray-400 text-xl font-semibold">
-          I’m a dedicated developer with four years of experience, specializing
-          in React and frontend development, with a strong focus on delivering
-          clean, efficient code. Ready to tackle new challenges and create
-          innovative web solutions!
+          As a passionate developer, I have gained valuable experience in
+          implementing new features and maintaining code across various
+          projects. I am dedicated to delivering high-quality, efficient solutions that
+          contribute to project success, and I thrive in collaborative
+          environments where I can help build exceptional products.
         </h3>
 
         <Separator className="my-12 h-[2px] bg-gray-500" />
 
         <h3 className="text-2xl font-semibold mb-6">Work Experience</h3>
         <div className="space-y-8 relative before:absolute before:inset-0 before:left-1/2 before:-translate-x-px before:h-full before:w-0.5 before:bg-gray-200">
-
-          <TimelineItem
-            date="03-2024"
-            isLeft={true}
-            isLatest={true}
-          >
+          <TimelineItem date="03-2024" isLeft={true} isLatest={true}>
             <p className="mt-1 text-gray-300">
               Started working as a Frontend Freelancer
             </p>
           </TimelineItem>
-          <TimelineItem
-            date="01-2023"
-            isLeft={false}
-          >
+          <TimelineItem date="01-2023" isLeft={false}>
             <p className="mt-1 text-gray-300">
               Working as a Frontend Developer <b>@Farfetch</b>
             </p>
@@ -88,7 +90,7 @@ export default function Home() {
           </TimelineItem>
           <TimelineItem date="10-2021" isLeft={false}>
             <p className="mt-1 text-gray-300">
-              Started working as a Frontend Developer <b>@OlympusDAO</b>
+              Started a job <b>@OlympusDAO</b> as a Frontend Developer
             </p>
           </TimelineItem>
           <TimelineItem date="01-2020" isLeft={true}>
@@ -98,9 +100,15 @@ export default function Home() {
           </TimelineItem>
         </div>
 
+        {/*         <Separator className="my-12 h-[2px] bg-gray-500" />
+
+        <Tech /> */}
+
         <Separator className="my-12 h-[2px] bg-gray-500" />
 
-        <Tech />
+        <div className="px-10 pb-10 pt-4">
+          <IconCloud iconSlugs={slugs} />
+        </div>
       </div>
     </div>
   );
@@ -145,14 +153,17 @@ function TimelineItem({
   );
 }
 
-function Tech() {
+/* function Tech() {
   const technologies = [
     { name: "Typescript", icon: <IconBrandTypescript size={54} /> },
     { name: "Javascript", icon: <IconBrandJavascript size={54} /> },
     { name: "React", icon: <IconBrandReact size={54} /> },
-    { name: "NextJS", icon: <IconBrandNextjs size={54} /> },
     { name: "HTML", icon: <IconBrandHtml5 size={54} /> },
     { name: "CSS", icon: <IconBrandCss3 size={54} /> },
+    { name: "NextJS", icon: <IconBrandNextjs size={54} /> },
+    { name: "NodeJS", icon: <IconBrandNodejs size={54} /> },
+    { name: ".NET", icon: <IconBrandCSharp size={54} /> },
+    { name: "AWS", icon: <IconBrandAws size={54} /> },
     { name: "Tailwind", icon: <IconBrandTailwind size={54} /> },
     { name: "GraphQL", icon: <IconBrandGraphql size={54} /> },
     { name: "Git", icon: <IconBrandGit size={54} /> },
@@ -172,6 +183,35 @@ function Tech() {
           </div>
         ))}
       </div>
+    </div>
+  );
+} */
+
+export function TechCloud() {
+  const slugs = [
+    "typescript",
+    "javascript",
+    "react",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "nextdotjs",
+    "prisma",
+    "amazonaws",
+    "vercel",
+    "testinglibrary",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "gitlab",
+  ];
+
+  return (
+    <div className="relative flex size-full max-w-lg items-center justify-center overflow-hidden rounded-lg border bg-background px-20 pb-20 pt-8 ">
+      <IconCloud iconSlugs={slugs} />
     </div>
   );
 }
